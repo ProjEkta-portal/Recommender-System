3eeb5f7686b8db1a968beacacac8f0f48ab01189
Neerajjr11 <neerajjm12345@gmail.com>
2023-09-23 10:24:06 +0530

    FastAPI

--- app.py
+++ app.py
@@ -1,51 +1,44 @@
-from flask import Flask, request, jsonify
-from pymongo import MongoClient
-from dotenv import load_dotenv
-import requests
-import os
-load_dotenv()
+from fastapi import FastAPI, HTTPException, Request
+import httpx
 
-# PASSWORD = os.getenv('PASS')
-app = Flask(__name__)
+app = FastAPI()
 
+# You don't need the dotenv library in FastAPI. Environment variables can be accessed directly.
 
-# data = requests.get("http://localhost:5000/api/projects")
+# data = httpx.get("http://localhost:5000/api/projects")
 # projects = data.json()
-# Get HTTP request from frontend
-@app.route("/recommend-projects", methods=["POST"])
-def get_projects():
-#   # Get JSON body from request
-  json_body = request.get_json()
 
-  # Extract skills from JSON body
-  skills = json_body["skills"]
-  projects = []
-  unique_project_names = set()
+@app.post("/recommend-projects")
+async def get_projects(request: Request):
+    # Get JSON body from request
+    json_body = await request.json()
 
-  # Make a GET request to the backend API to fetch project data
-  # backend_api_url = "http://localhost:5000/api/projects"
-  backend_api_url = "https://eager-bass-sombrero.cyclic.cloud/api/projects"
-  response = requests.get(backend_api_url)
+    # Extract skills from JSON body
+    skills = json_body["skills"]
+    projects = []
+    unique_project_names = set()
 
-  # Check if the API request was successful
-  if response.status_code == 200:
-      project_data = response.json()
-      # Iterate through the skills and find 3 projects for each skill
-      for skill in skills:
-          skill_projects = [project for project in project_data if skill in project["tags"]]
-          skill_projects = skill_projects[:3]  # Limit to 3 projects per skill
+    # Make a GET request to the backend API to fetch project data
+    backend_api_url = "https://eager-bass-sombrero.cyclic.cloud/api/projects"
+    
+    async with httpx.AsyncClient() as client:
+        response = await client.get(backend_api_url)
 
-          # Add the projects to the list, making sure to avoid duplicates
-          for project in skill_projects:
-              if project["name"] not in unique_project_names:
-                  projects.append(project)
-                  unique_project_names.add(project["name"])
-  else:
-      # return 0
-      return jsonify({"error": "Failed to fetch project data from the backend API"}), 500
+    # Check if the API request was successful
+    if response.status_code == 200:
+        project_data = response.json()
+        # Iterate through the skills and find 3 projects for each skill
+        for skill in skills:
+            skill_projects = [project for project in project_data if skill in project["tags"]]
+            skill_projects = skill_projects[:3]  # Limit to 3 projects per skill
 
-  # Return the list of projects
-  return jsonify({"recommended_projects": projects})
+            # Add the projects to the list, making sure to avoid duplicates
+            for project in skill_projects:
+                if project["name"] not in unique_project_names:
+                    projects.append(project)
+                    unique_project_names.add(project["name"])
+    else:
+        raise HTTPException(status_code=500, detail="Failed to fetch project data from the backend API")
 
-if __name__ == "__main__":
-  app.run(debug=True)
+    # Return the list of projects
+    return {"recommended_projects": projects}
